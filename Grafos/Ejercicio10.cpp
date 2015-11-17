d1b99350c6f58bde91b0d41e24a130a252df895a
Ruben <Ruben@192.168.1.36>
2015-11-17 16:21:35 +0100

    ejer10

--- Grafos/Ejercicio10.cpp
+++ Grafos/Ejercicio10.cpp
@@ -4,34 +4,12 @@
  * and open the template in the editor.
  */
 
-
-#include <time.h>
-
 #include "Grafo.h"
 #include "ArbolLibre.h"
 
-void dfsAux(Grafo const& G, size_t v,bool marked[],size_t &count) 
-{
-    ++count;
-    marked[v] = true;
-    for (auto w : G.adj(v)) {
-        if (!marked[w]) {
-            dfsAux(G,w,marked,count);
-        }
-    }
-}
 
-size_t dfs(Grafo const& G, size_t v)
+bool resuelveCaso() 
 {
-    bool *marked = new bool[G.V()];
-    size_t count = 0;
-    dfsAux(G,v,marked,count);
-    
-    return count;
-    
-}
-
-bool resuelveCaso() {
     int V, A, v, w;
     std::cin >> V; // número de vértices
     if (!std::cin)  // hemos terminado de procesar todos los casos
@@ -39,22 +17,23 @@ bool resuelveCaso() {
     std::cin >> A; // número de aristas
     Grafo grafo(V);
     // leemos las aristas
-    for (int i = 0; i < A; ++i) {
+    for (int i = 0; i < A; ++i) 
+    {
         std::cin >> v >> w;
         grafo.ponArista(v,w);
     }
     
     ArbolLibre arbol = ArbolLibre(grafo);
-    if (arbol.grafoLibre())
+    
+    if(arbol.grafoLibre())
         std::cout << "SI\n";
     else
         std::cout << "NO\n";
+    
+    return true;
 }
 int main()
 {
-    while(resuelveCaso())
-    {
-        
-    }
+    while(resuelveCaso());
     return 0;
 }
