707b9cc203e2c4d6484243d4fc35a0dee7377a54
Ruben <Ruben@192.168.1.36>
2015-11-17 15:58:56 +0100

    ejer10

--- Grafos/ArbolLibre.h
+++ Grafos/ArbolLibre.h
@@ -14,6 +14,85 @@
 #ifndef ARBOLLIBRE_H
 #define ARBOLLIBRE_H
 
+#include <unordered_map>
+#include "Grafo.h"
+
+class ArbolLibre{
+    
+private:
+    bool _esLibre;
+    bool _esCiclico;
+    bool _esConexo;
+    
+public:
+    
+    ArbolLibre(const Grafo &g)
+    {
+        _esCiclico = false;
+        _esConexo = dfs(g,0,_esCiclico) == g.V();
+        _esLibre = !_esCiclico && _esConexo;
+        
+    }
+    
+    bool grafoLibre(){
+        return _esLibre;
+    }
+    
+private:
+    
+    void dfsAux(Grafo const& G, size_t v,bool marked[],size_t &count,int p, bool &hayCiclo) 
+    {
+        if (!hayCiclo)
+        {
+            ++count;
+            marked[v] = true;
+            
+            for (auto w : G.adj(v)) 
+            {
+                
+                if (w != p) 
+                {
+                    if(!marked[w])
+                        dfsAux(G,w,marked,count,p,hayCiclo);
+                    else
+                        hayCiclo = true;
+                }
+            }
+        }
+    }
+
+    size_t dfs(Grafo const& G, size_t v, bool & esCiclico)
+    {
+        bool *marked = new bool[G.V()];
+        size_t count = 0;
+        dfsAux(G,v,marked,count,v,esCiclico);
+
+        return count;
+
+    }
+    /*
+    void grafoCiclico(const Grafo &g,int v, int p, bool &hayCiclo, std::unordered_map<int,bool> &visitados)
+    {
+        
+        if (!hayCiclo)
+        {
+            for (auto w : g.adj(v))
+            {
+                if (w != p)
+                {
+                    hayCiclo = visitados.find(w) != visitados.end();
+                    if (!hayCiclo)
+                    {
+                        grafoCiclico(g,w,v,hayCiclo);
+                    }
+                }
+            }
+        }
+        
+        
+    }
+    */
+};
 
 
 #endif /* ARBOLLIBRE_H */
--- Grafos/Ejercicio10.cpp
+++ Grafos/Ejercicio10.cpp
@@ -5,15 +5,56 @@
  */
 
 
-#include "Grafo.h"
+#include <time.h>
 
+#include "Grafo.h"
+#include "ArbolLibre.h"
+
+void dfsAux(Grafo const& G, size_t v,bool marked[],size_t &count) 
+{
+    ++count;
+    marked[v] = true;
+    for (auto w : G.adj(v)) {
+        if (!marked[w]) {
+            dfsAux(G,w,marked,count);
+        }
+    }
+}
+
+size_t dfs(Grafo const& G, size_t v)
+{
+    bool *marked = new bool[G.V()];
+    size_t count = 0;
+    dfsAux(G,v,marked,count);
+    
+    return count;
+    
+}
+
+bool resuelveCaso() {
+    int V, A, v, w;
+    std::cin >> V; // número de vértices
+    if (!std::cin)  // hemos terminado de procesar todos los casos
+        return false;
+    std::cin >> A; // número de aristas
+    Grafo grafo(V);
+    // leemos las aristas
+    for (int i = 0; i < A; ++i) {
+        std::cin >> v >> w;
+        grafo.ponArista(v,w);
+    }
+    
+    ArbolLibre arbol = ArbolLibre(grafo);
+    if (arbol.grafoLibre())
+        std::cout << "SI\n";
+    else
+        std::cout << "NO\n";
+}
 int main()
 {
-    std::string fichero = "Grafos/Ejercicio10.txt";
-    Grafo grafo = Grafo(fichero);
-    
-    grafo.print();
-    std::cout << "hola";
-    
+    while(resuelveCaso())
+    {
+        
+    }
     return 0;
 }
