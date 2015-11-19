2fe3f0c9ff5ae9e7a94eadddc5aaf27887be15df
Ruben <Ruben@192.168.1.36>
2015-11-19 18:00:38 +0100

    Cambiando ejer12

--- Grafos/Arborescencia.h
+++ Grafos/Arborescencia.h
@@ -56,33 +56,27 @@ public:
     
 private:
     
-    void dfsAux(GrafoDirigido const& G, size_t v,std::vector<bool> &marked,size_t &count,size_t p, bool &hayCiclo)
+    void dfsAux(GrafoDirigido const& G, size_t v,std::vector<bool> &marked,size_t p, bool &hayCiclo)
     {
         if (!hayCiclo)
         {
-            ++count;
             marked[v] = true;
             
             for (auto w : G.adj(v))
             {
-                if (w != p)
-                {
-                    if(!marked[w])
-                        dfsAux(G,w,marked,count,v,hayCiclo);
-                    else
-                        hayCiclo = true;
+                if(!marked[w])
+                    dfsAux(G,w,marked,v,hayCiclo);
+                else
+                    hayCiclo = true;
                 }
             }
-        }
     }
     
-    size_t dfs(GrafoDirigido const& G, size_t v, bool & esCiclico)
+    
+    void dfs(GrafoDirigido const& G, size_t v, bool & esCiclico)
     {
         std::vector<bool> marked(G.V());
-        size_t count = 0;
-        dfsAux(G,v,marked,count,v,esCiclico);
-        
-        return count;
+        dfsAux(G,v,marked,v,esCiclico);
         
     }
     
