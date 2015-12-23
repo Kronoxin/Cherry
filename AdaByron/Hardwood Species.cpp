6ac25e8f978ab366eb79e873a2b581c0ff382643
Ruben <Ruben@10.211.55.3>
2015-12-23 10:50:21 +0000

    Hardwood Species CORRECT (Entrenamiento AdaByron)

--- AdaByron/Hardwood Species.cpp
+++ AdaByron/Hardwood Species.cpp
@@ -1,13 +1,12 @@
-//
-//  Hardwood Species.cpp
-
-
-#include <stdio.h>
 #include <iostream>
-#include <vector>
+#include <unordered_map>
+#include <map>
+#include <string>
+#include <iomanip>
+using namespace std;
 
 
-bool resuelveCaso()
+void resuelveCaso()
 {
     int nCasos;
     char c ;
@@ -16,32 +15,52 @@ bool resuelveCaso()
     std::cin >> nCasos ; // leo el numero de casos.
     std::cin.get(c);     //leo el \n
     std::cin.get(c);     // y otra vez
-
+    
     
     std::string especie;
     
     
-    for ( int i = 0 ; i< nCasos; i++){
-        std::vector<std::string> vector(0);
-    
-        do {
+    for ( int i = 0 ; i< nCasos; i++)
+    {
+        unordered_map<string,long> conteoEspecies;
+        map<string,short> nombreEspecies;
+        long numeroArboles = 0;
+        do
+        {
             
             std::getline(std::cin, especie);
             if(especie !="")
-                vector.push_back(especie);
+            {
+                if (conteoEspecies.find(especie) != conteoEspecies.end())
+                    conteoEspecies[especie]++;
+                else
+                {
+                    conteoEspecies[especie] = 1; // O(1)
+                    nombreEspecies[especie] = 1; // O(log n)
+                }
+                numeroArboles++;
+            }
+            
+        }
+        while (especie !="");
         
-        } while (especie !="");
+        map<string,short>::const_iterator it = nombreEspecies.cbegin();
+        while(it != nombreEspecies.end())
+        {
+            float porcentaje = (float)conteoEspecies[it->first]/numeroArboles*100; // Sacamos el porcentaje.
+            std::cout << it->first << " " << std::fixed << std::setprecision(4) << porcentaje << std::endl; // Dejamos 4 decimales.
+            it++;
+        }
+        if (i != nCasos-1)
+            std::cout << endl;
+        
     }
-    
-    
-    
-    return true;
     
 }
 
 
 int main()
 {
-    while(resuelveCaso());
+    resuelveCaso();
     return 0;
 }
